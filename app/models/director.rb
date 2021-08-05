13e98c05ffc158cb95e9484f44ca8d8206d70cff
s-b-edu <sbaur@chicagobooth.edu>
2021-08-05 01:40:12 +0000

    done

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,11 +11,14 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
-  def filmography
-    key = self.id
+  has_many(:filmography, {
+    :class_name => "Movie"
+  })
+  # def filmography
+  #   key = self.id
 
-    the_many = Movie.where({ :director_id => key })
+  #   the_many = Movie.where({ :director_id => key })
 
-    return the_many
-  end
+  #   return the_many
+  # end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,13 +13,14 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
-  def director
-    key = self.director_id
+  belongs_to(:director)
+  # def director
+  #   key = self.director_id
 
-    matching_set = Director.where({ :id => key })
+  #   matching_set = Director.where({ :id => key })
 
-    the_one = matching_set.at(0)
+  #   the_one = matching_set.at(0)
 
-    return the_one
-  end
+  #   return the_one
+  # end
 end
